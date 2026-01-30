aa0ba2814a91d1b8327637f19e7b038f74aa9c7c
shaggysa <gabriel.couchenour@gmail.com>
2026-01-29 21:54:19 -0600

    refactored commands and event listener to avoid an unnecessary companion object

--- src/main/kotlin/xyz/shaggysa/stillSky/StillSky.kt
+++ src/main/kotlin/xyz/shaggysa/stillSky/StillSky.kt
@@ -8,48 +8,48 @@ import org.bukkit.event.player.PlayerJoinEvent
 import org.bukkit.event.player.PlayerQuitEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-fun serverEmpty(): Boolean {
-    return (Bukkit.getOnlinePlayers().isEmpty())
-}
 
-fun freezeDaylight() {
-    Bukkit.getWorlds()[0].setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)
-    StillSky.instance.logger.info("The daylight cycle has paused.")
-}
 
-fun unfreezeDaylight() {
-    Bukkit.getWorlds()[0].setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true)
-    StillSky.instance.logger.info("The daylight cycle has resumed.")
-}
-
-class PlayerListener : Listener {
+class PlayerListener(private val plugin: StillSky) : Listener {
     @EventHandler
+    @Suppress("UNUSED_PARAMETER")
     fun onPlayerQuit(event: PlayerQuitEvent) {
-        if (serverEmpty() || Bukkit.getOnlinePlayers().size == 1) { //playercount updates after event listener
-            freezeDaylight()
+        // player count updates after the event listener triggers in this case
+        if (plugin.serverEmpty() || Bukkit.getOnlinePlayers().size == 1) {
+            plugin.freezeDaylight()
         }
     }
 
     @EventHandler
+    @Suppress("UNUSED_PARAMETER")
     fun onPlayerJoin(event: PlayerJoinEvent) {
         if (Bukkit.getOnlinePlayers().size == 1) {
-            unfreezeDaylight()
+            plugin.unfreezeDaylight()
         }
     }
 }
 
 class StillSky : JavaPlugin() {
-    companion object {
-        val instance: StillSky by lazy {
-            Bukkit.getPluginManager().getPlugin("StillSky") as StillSky
-        }
+
+    fun serverEmpty(): Boolean {
+        return (Bukkit.getOnlinePlayers().isEmpty())
+    }
+
+    fun freezeDaylight() {
+        Bukkit.getWorlds()[0].setGameRule(GameRule.DO_DAYLIGHT_CYCLE, false)
+        logger.info("The daylight cycle has paused.")
+    }
+
+    fun unfreezeDaylight() {
+        Bukkit.getWorlds()[0].setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true)
+        logger.info("The daylight cycle has resumed.")
     }
 
     override fun onEnable() {
         if (serverEmpty()) {
             freezeDaylight()
         }
-        server.pluginManager.registerEvents(PlayerListener(), this)
+        server.pluginManager.registerEvents(PlayerListener(this), this)
     }
 
     override fun onDisable() {
